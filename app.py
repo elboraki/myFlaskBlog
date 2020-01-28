4c37367338ea575b85b8007368b9f547ecf6f8d7
elboraki <burak7younes@gmail.com>
2020-01-28 14:46:06 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Index Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>about Page</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
