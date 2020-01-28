82a02cb103ae4059e2c74acf389e704eac770069
elboraki <burak7younes@gmail.com>
2020-01-28 16:46:25 +0100

    create a template for app

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
-
+posts=[
+    {
+        'author':"younes",
+        'title':'Blog title 1',
+        'content':"content blog 1",
+        'date_posted':"april 20,2018",
+    },
+        {
+        'author':"Hicham",
+        'title':'Blog title 2',
+        'content':"content blog 2",
+        'date_posted':"june 20,2018",
+    },
+]
 @app.route("/")
 def index():
-    return "<h1>Index Home Page</h1>"
+    return render_template("home.html",posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>about Page</h1>"
+     return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
