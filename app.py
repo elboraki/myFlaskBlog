acaa84927eac5d2877d45c1147c8c28a17436015
elboraki <younes.elboraki@gmail.com>
2020-01-29 23:41:10 +0100

    create form and template for registration and authentification

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import LoginForm,RegistrationForm
 app=Flask(__name__)
 
+app.config['SECRET_KEY']="e3bb04e3fcf9d0e6f0df7feaeed28f96"
 posts=[
     {
         'author':"younes",
@@ -17,13 +18,32 @@ posts=[
     },
 ]
 @app.route("/")
-def index():
+def home():
     return render_template("home.html",posts=posts)
 
 
 @app.route("/about")
 def about():
-     return render_template("about.html")
+    return render_template("about.html")
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html",title="Registration",form=form)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=='younes@self.me' and form.password.data=='123123':
+            flash('You have been looged in',"success")
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html",title="Login",form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
