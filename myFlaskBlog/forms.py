a6fcd550224170797e6ae46743a110704739b20b
elboraki <younes.elboraki@gmail.com>
2020-02-06 00:57:42 +0100

    add login and logout

--- myFlaskBlog/forms.py
+++ myFlaskBlog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Length,Email,EqualTo
+from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
+from myFlaskBlog.models import User
 
 class RegistrationForm(FlaskForm):
     username=StringField("Username",validators=[DataRequired(),Length(min=2,max=20)])
@@ -9,6 +10,17 @@ class RegistrationForm(FlaskForm):
     confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo('password')])
     submit=SubmitField('Register')
 
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user :
+            raise ValidationError("this username is taken, Please choose a diffrents one")
+    
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user :
+            raise ValidationError("this email is taken, Please choose a diffrents one")
+
+        
 
 class LoginForm(FlaskForm):
     email=StringField("Email",validators=[DataRequired(),Email()])
