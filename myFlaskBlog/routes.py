d189119a81f05ca0d90e10cd87d2ccedb3b5a17b
elboraki <younes.elboraki@gmail.com>
2020-02-07 23:00:37 +0100

    create account feature to modify data and upload picture

--- myFlaskBlog/routes.py
+++ myFlaskBlog/routes.py
@@ -1,7 +1,10 @@
 
+import os
+import secrets
+from PIL import Image
 from myFlaskBlog import app,bcrypt,db
 from flask import  render_template, url_for, flash, redirect,request
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm,UpdateAccountForm
 from .models import Post,User
 from flask_login import current_user,logout_user,login_required,login_user
 
@@ -62,10 +65,38 @@ def login():
     return render_template("login.html", title="Login", form=form)
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account",methods=["GET","POST"])
 @login_required
 def account():
-    return render_template("account.html",title="account")
+    form=UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file=picture_file
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash("Your account has been updated","success")
+        return redirect(url_for("account"))
+    elif request.method=='GET':
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+    image_file=url_for('static',filename='profile_pics/'+current_user.image_file)
+    return render_template("account.html",title="account",image_file=image_file,form=form)
 
 @app.route("/logout")
 def logout():
